ed22f70deba96e4c3f1573284a79e32f5be4e005
Oliver343 <93383676+Oliver343@users.noreply.github.com>
2022-02-25 11:38:15 +0100

    Changed user page

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 from unicodedata import name
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ def home():
 
 @app.route("/<name>")
 def user(name):
-    return f"Hello {name}!"
+    return f"""Hello {name}! \n <marquee behavior="alternate" direction="down" height="600" scrollamount="20" style="border:solid" width="800"> <marquee behavior="alternate"> <p><a href="/">Welcome</a></p> </marquee> </marquee></p> </div>" """
 
 
 if __name__ == "__main__":
